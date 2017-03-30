b159e2828e8fd9e7bfc5f5f40ba39f656045da4a
Bhaargavi Agrawal <bhaargavi.127@gmail.com>
2017-03-30 23:37:13 +0530

    Update basics.php

--- templates/basics.php
+++ templates/basics.php
@@ -1,49 +1,17 @@
-/* Creating a sample of metaboxes 
-Then the other file contains the code to add, format and save these . 
 <?php
-include("inventor-school/templates/main.php");
-
-//I created an array called $meta_box and set the array key to the relevant post type, in this case post
-add_action( 'init', 'prefix_create_metaboxes' );
-function prefix_create_metaboxes() {
-$meta_boxes = array();
-$meta_boxes[] = array(
-    'id' => 'Place',  
-    'title' => 'place',
-    'pages' => array('post'), // post type
-    'context' => 'normal',
-    'priority' => 'high',
-     array(
-        'name' => 'District',
-        'desc' => 'district_name',
-        'id' => 'dis_name',
-        'type' => 'text',
-        'default' => ''
-    ),
-    array(
-        'name' => 'Block',
-        'desc' => 'block_name',
-        'id' => 'blk_name',
-        'type' => 'text',
-        'default' => ''
-    ),
-    array(
-        'name' => 'Cluster',
-        'desc' => 'cluster_name',
-        'id' => 'clstr_name',
-        'type' => 'text',
-        'default' => ''
-    ),
-    array(
-        'name' => 'Village',
-        'desc' => 'village_name',
-        'id' => 'vilg_name',
-        'type' => 'text',
-        'default' => ''
-    )
-)
-);
-
+add_action( 'add_meta_boxes', 'cd_meta_box_add' );
+function cd_meta_box_add()
+{
+    add_meta_box( 'place', 'Place_Name', 'callback_place', 'post', 'normal', 'high' );
 }
-add_action('add_meta_boxes', 'plib_add_box');
+?>
+<?php
+function callback_place( $post )
+{
+$values = get_post_custom( $post->ID );
+$district = isset( $values['dis_name'] ) ? esc_attr( $values['dis_name'][0] ) : ”;
+$block = isset( $values['block_name'] ) ? esc_attr( $values['block_name'][0] ) : ”;
+$cluster = isset( $values['clust_name'] ) ? esc_attr( $values['clust_name'][0] ) : ”;
+$village = isset( $values['vilg_name'] ) ? esc_attr( $values['vilg_name'][0] ) : ”;
+wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
 ?>
