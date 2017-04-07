abcbb763a0386b82849c2bfe8b578207be2ee684
Bhaargavi Agrawal <bhaargavi.127@gmail.com>
2017-04-07 17:13:50 +0530

    Final Working Code

--- templates/valuation.php
+++ templates/valuation.php
@@ -1,18 +1,18 @@
 <!-- VALUATION -->
-<?php do_action( 'inventor_before_listing_detail_property_valuation' ); ?>
+<?php do_action( 'inventor_before_listing_detail_school_valuation' ); ?>
 
-<?php $valuation = get_post_meta( get_the_ID(), INVENTOR_LISTING_PREFIX . INVENTOR_PROPERTY_PREFIX . 'valuation', true ); ?>
+<?php $valuation = get_post_meta( get_the_ID(), INVENTOR_LISTING_PREFIX . INVENTOR_SCHOOL_PREFIX . 'valuation', true ); ?>
 
 <?php if ( ! empty( $valuation ) && is_array( $valuation ) && ! empty( $valuation[0] ) ) : ?>
-    <div class="listing-detail-section" id="listing-detail-section-property-valuation">
-        <h2 class="page-header"><?php echo __( 'Valuation', 'inventor-properties' ); ?></h2>
+    <div class="listing-detail-section" id="listing-detail-section-school-valuation">
+        <h2 class="page-header"><?php echo __( 'Valuation', 'inventor-schools' ); ?></h2>
 
-        <div class="listing-detail-property-valuation">
+        <div class="listing-detail-school-valuation">
             <?php foreach ( $valuation as $v ) : ?>
-                <?php $key = esc_attr( $v[ INVENTOR_LISTING_PREFIX . INVENTOR_PROPERTY_PREFIX . 'valuation_key' ] ); ?>
-                <?php $value = esc_attr( $v[ INVENTOR_LISTING_PREFIX . INVENTOR_PROPERTY_PREFIX . 'valuation_value' ] ); ?>
+                <?php $key = esc_attr( $v[ INVENTOR_LISTING_PREFIX . INVENTOR_SCHOOL_PREFIX . 'valuation_key' ] ); ?>
+                <?php $value = esc_attr( $v[ INVENTOR_LISTING_PREFIX . INVENTOR_SCHOOL_PREFIX . 'valuation_value' ] ); ?>
 
-                <div class="listing-detail-property-valuation-item">
+                <div class="listing-detail-school-valuation-item">
                     <dt><?php echo $key; ?></dt>
                     <dd>
                         <div class="bar-valuation"
@@ -21,10 +21,10 @@
                         </div>
                     </dd>
                     <span class="percentage-valuation"><?php echo $value; ?> %</span>
-                </div><!-- /.property-valuation-item -->
+                </div><!-- /.school-valuation-item -->
             <?php endforeach; ?>
-        </div><!-- /.listing-detail-property-valuation-->
+        </div><!-- /.listing-detail-school-valuation-->
     </div><!-- /.listing-detail-section -->
 <?php endif; ?>
 
-<?php do_action( 'inventor_after_listing_detail_property_valuation' ); ?>
+<?php do_action( 'inventor_after_listing_detail_school_valuation' ); ?>
